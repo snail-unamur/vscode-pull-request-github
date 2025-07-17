4db78bd80bb2790c00a104afc122bd8de2756c39
Hugo Raskin <raskin.hugo@gmail.com>
2025-07-17 15:53:19 +0200

    add yarn lock + fix test

--- webviews/editorWebview/test/builder/pullRequest.ts
+++ webviews/editorWebview/test/builder/pullRequest.ts
@@ -60,5 +60,5 @@ export const PullRequestBuilder = createBuilderClass<PullRequest>()({
 	lastReviewType: { default: undefined },
 	canAssignCopilot: { default: false },
 	reactions: { default: [] },
-	analysis: { default: { riskValue: 0, riskCategory: 'A', metrics: []} },
+	analysis: { default: { riskValue: 0, riskCategory: 'A', radarMetrics: [] } },
 });
